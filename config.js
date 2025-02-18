e5019094766de619489beedddbe903651db67a19
hxgdsjzuyt1234hg <jaihacku@gmail.com>
2025-02-18 20:42:45 +0530

    Update config.js

--- config.js
+++ config.js
@@ -1,4 +1,4 @@
 module.exports = {
-  BOT_TOKEN: "null",
-    allowedDevelopers: ['null'], // ID
+  BOT_TOKEN: "7776286968:AAEv-aNlU21h8E2uEiHiAsj8mY7PEVqA2ks",
+    allowedDevelopers: ['7430156268'], // ID
 };
